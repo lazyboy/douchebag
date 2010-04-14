4da153b5c3e48a1471c153300c89ac3e26d79278
lazyboybd <debasis@roy-debasis.(none)>
2010-04-15 00:01:21 +0600

    Added sql schema for database tables

---
+++ douchebag.sql
@@ -0,0 +1,7 @@
+﻿CREATE DATABASE `douchebag` DEFAULT CHARSET=utf8;
+
+CREATE TABLE  `douchebag`.`path` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `p` varchar(3000) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;

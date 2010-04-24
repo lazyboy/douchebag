eef8c3e3604208d80d6f1c638a1fcdb10a7c8713
Debasis Roy <debasisroy144@gmail.com>
2010-04-25 00:32:37 +0600

    Added slf4j dependency in pom, Moved DouchebagServlet to testing packate, Added more tables in sql script

--- douchebag.sql
+++ douchebag.sql
@@ -1,7 +1,46 @@
-﻿CREATE DATABASE `douchebag` DEFAULT CHARSET=utf8;
+CREATE DATABASE `douchebag` DEFAULT CHARSET=utf8;
 
-CREATE TABLE  `douchebag`.`path` (
+CREATE TABLE  `path` (
   `id` int(10) unsigned NOT NULL auto_increment,
-  `p` varchar(3000) NOT NULL,
+  `p` text NOT NULL,
   PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE  `sin` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `description` text NOT NULL,
+  `start` int(11) NOT NULL default '-1',
+  `end` int(11) NOT NULL default '-1',
+  `length` int(11) NOT NULL default '-1',
+  `rate` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE  `droid` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `type` int(11) NOT NULL default '0',
+  `name` text NOT NULL,
+  `description` text NOT NULL,
+  `tags` text NOT NULL,
+  `url` text NOT NULL,
+  `localurl` text NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE  `sin_path` (
+  `fk_sin_id` int(10) unsigned NOT NULL,
+  `fk_path_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`fk_sin_id`,`fk_path_id`),
+  KEY `fk_sin_path_path_id` (`fk_path_id`),
+  CONSTRAINT `fk_sin_path_sin_id` FOREIGN KEY (`fk_sin_id`) REFERENCES `sin` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_sin_path_path_id` FOREIGN KEY (`fk_path_id`) REFERENCES `path` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE  `droid_sin` (
+  `fk_droid_id` int(10) unsigned NOT NULL,
+  `fk_sin_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`fk_droid_id`,`fk_sin_id`),
+  KEY `fk_droid_sin_sin_id` (`fk_sin_id`),
+  CONSTRAINT `fk_droid_sin_droid_id` FOREIGN KEY (`fk_droid_id`) REFERENCES `droid` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_droid_sin_sin_id` FOREIGN KEY (`fk_sin_id`) REFERENCES `sin` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
